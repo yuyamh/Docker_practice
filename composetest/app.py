64268278c9d7a3987e125b4fb4a1d30151741fcc
yuyamh <yuya.htr0828@gmail.com>
2022-10-31 23:54:36 +0900

    webにボリュームを追加

--- composetest/app.py
+++ composetest/app.py
@@ -7,4 +7,4 @@ cache = redis.Redis(host='redis', port=6379)
 @app.route('/')
 def index():
   count = cache.incr('hits')
-  return 'Hello world! I have been seen {} times\n'.format(count)
+  return 'Hello! I have been seen {} times\n'.format(count)
